e5e0f448b42e4bf14ac4fab062d08d48b1efee58
zaakyyz <muhammadzaky034@gmail.com>
2026-06-09 21:37:35 +0700

    feat: implement draw_barcode_boundary function for barcode visualization

--- QrBarTest.py
+++ QrBarTest.py
@@ -2,6 +2,20 @@ import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
 
+
+def draw_barcode_boundary(img, barcode, color):
+    x, y, w, h = barcode.rect
+
+    if barcode.type == 'QRCODE' and len(barcode.polygon) >= 4:
+        pts = np.array([barcode.polygon], np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        cv2.polylines(img, [pts], True, color, 5)
+    else:
+        cv2.rectangle(img, (x, y), (x + w, y + h), color, 5)
+
+    return x, y
+
+
 #img = cv2.imread('1.png')
 cap = cv2.VideoCapture(0)
 cap.set(3,640)
@@ -13,11 +27,8 @@ while True:
     for barcode in decode(img):
         myData = barcode.data.decode('utf-8')
         print(myData)
-        pts = np.array([barcode.polygon],np.int32)
-        pts = pts.reshape((-1,1,2))
-        cv2.polylines(img,[pts],True,(255,0,255),5)
-        pts2 = barcode.rect
-        cv2.putText(img,myData,(pts2[0],pts2[1]),cv2.FONT_HERSHEY_SIMPLEX, 0.9,(255,0,255),2)
+        x, y = draw_barcode_boundary(img, barcode, (255,0,255))
+        cv2.putText(img,myData,(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.9,(255,0,255),2)
 
     cv2.imshow('Result',img)
     key = cv2.waitKey(1)
--- QrCodeProject.py
+++ QrCodeProject.py
@@ -2,6 +2,20 @@ import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
 
+
+def draw_barcode_boundary(img, barcode, color):
+    x, y, w, h = barcode.rect
+
+    if barcode.type == 'QRCODE' and len(barcode.polygon) >= 4:
+        pts = np.array([barcode.polygon], np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        cv2.polylines(img, [pts], True, color, 5)
+    else:
+        cv2.rectangle(img, (x, y), (x + w, y + h), color, 5)
+
+    return x, y
+
+
 #img = cv2.imread('1.png')
 cap = cv2.VideoCapture(0)
 cap.set(3,640)
@@ -24,11 +38,8 @@ while True:
             myOutput = 'Un-Authorized'
             myColor = (0, 0, 255)
 
-        pts = np.array([barcode.polygon],np.int32)
-        pts = pts.reshape((-1,1,2))
-        cv2.polylines(img,[pts],True,myColor,5)
-        pts2 = barcode.rect
-        cv2.putText(img,myOutput,(pts2[0],pts2[1]),cv2.FONT_HERSHEY_SIMPLEX,
+        x, y = draw_barcode_boundary(img, barcode, myColor)
+        cv2.putText(img,myOutput,(x,y),cv2.FONT_HERSHEY_SIMPLEX,
                     0.9,myColor,2)
 
     cv2.imshow('Result',img)
